24d78c62c6790ec6ca2102c5a8e5c479e0179f9e
Astatine-213-Tian <astatine213.tian@gmail.com>
2023-06-14 23:43:11 -0400

    style: move provider into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 // app/layout.tsx
-import { Providers } from "./providers";
+"use client";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
+import { CacheProvider } from '@chakra-ui/next-js'
 
 export default function RootLayout({
   children,
@@ -9,9 +11,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <CacheProvider>
+          <ChakraProvider>
+              {children}
+          </ChakraProvider>
+        </CacheProvider>
       </body>
     </html>
   );
