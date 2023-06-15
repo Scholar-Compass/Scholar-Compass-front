a8483f587c6dbc8c9fd3386257b7f7e7d7b4bbfb
Astatine-213-Tian <astatine213.tian@gmail.com>
2023-06-14 23:49:51 -0400

    perf: better css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,7 @@ import {
   Icon,
 } from '@chakra-ui/react';
 import {FaPaperPlane} from 'react-icons/fa';
-import RootLayout from "@/app/layout";
-import TitleBar from '@/components/TitleBar';
+import NavBar from '@/components/NavBar';
 
 function App() {
   const [inputText, setInputText] = useState('');
@@ -78,78 +77,66 @@ function App() {
   };
 
   return (
-    <RootLayout>
-       <Flex
-          minHeight="100vh"
-          direction="column"
-        >
-        <TitleBar height="50px"/>
-        <Box 
-          flex={1} 
-          pr={8} 
-          pl={8} 
-          pt={4}
-          pb={4}
-          mt="50px"
-          mb="80px"
-          overflowY="scroll"
-          maxHeight={`calc(100vh - 130px)`}
-        >
-          {messages.map((message, index) => (
-            <Flex
-              key={index}
-              justify={message.isUser ? 'flex-end' : 'flex-start'}
-              mt={2}
-              wordBreak={"break-word"}
+    <Flex minH="100vh" flexDir="column">
+      <NavBar/>
+      <Box
+        flex={1}
+        px={8} 
+        py={4}
+        overflowY="auto"
+      >
+        {messages.map((message, index) => (
+          <Flex
+            key={index}
+            justify={message.isUser ? 'flex-end' : 'flex-start'}
+            mt={2}
+            wordBreak={"break-word"}
+          >
+            <Box
+              bg={message.isUser ? 'blue.500' : 'gray.200'}
+              color={message.isUser ? 'white' : 'black'}
+              p={3}
+              borderRadius="lg"
+              boxShadow="md"
+              whiteSpace="pre-wrap"
+              maxWidth="70%"
             >
-              <Box
-                bg={message.isUser ? 'blue.500' : 'gray.200'}
-                color={message.isUser ? 'white' : 'black'}
-                p={3}
-                borderRadius="lg"
-                boxShadow="md"
-                whiteSpace="pre-wrap"
-                maxWidth="70%"
-              >
-                {message.content}
-              </Box>
-            </Flex>
-          ))}
+              {message.content}
+            </Box>
+          </Flex>
+        ))}
 
-          {isLoading && (
-            <Flex justify="flex-start" mt={4}>
-              <Spinner size="lg" color="blue.500" />
-            </Flex>
-          )}
-        </Box>
-
-        <Box 
-          mt={4} p={4} 
-          width="100%"
-          pos="fixed"
-          bottom={0}
-          zIndex={999}
-          h="80px"
-        >
-          <InputGroup>
-            <Input
-              placeholder="Type your question..."
-              value={inputText}
-              onChange={handleInputChange}
-              onKeyDown={handleKeyDown}
+        {isLoading && (
+          <Flex justify="flex-start" mt={4}>
+            <Spinner size="lg" color="blue.500" />
+          </Flex>
+        )}
+      </Box>
+      <Box 
+        p={4} 
+        pos="sticky"
+        bottom={0}
+        zIndex={999}
+        bg="white"
+      >
+        <InputGroup>
+          <Input
+            placeholder="Type your question..."
+            value={inputText}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+          <InputRightElement>
+            <Icon
+              as={FaPaperPlane}
+              color="blue.500"
+              cursor="pointer"
+              onClick={handleSendMessage}
             />
-            <InputRightElement>
-              <Icon
-                as={FaPaperPlane}
-                color="blue.500"
-                cursor="pointer"
-                onClick={handleSendMessage}
-              />
-            </InputRightElement>
-          </InputGroup>
-        </Box>
-      </Flex>
-    </RootLayout>
+          </InputRightElement>
+        </InputGroup>
+      </Box>
+    </Flex>
   );
 }
 
