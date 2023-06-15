fd86f008a8e132823e1b2d11ddcdc504f2107f89
Astatine-213-Tian <astatine213.tian@gmail.com>
2023-06-14 23:50:42 -0400

    feat: add initial system message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,41 @@ import {
 import {FaPaperPlane} from 'react-icons/fa';
 import NavBar from '@/components/NavBar';
 
-function App() {
-  const [inputText, setInputText] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [messages, setMessages] = useState<Message[]>([]);
+const systemMessage = 
+`Hi亲爱的同学们：
+欢迎使用ScholarCompass AI对话机器人 (beta)~
+你可以用以下问题开启对话：
+综合类：
+“我是一个比较擅长数学和物理的学生，可以推荐一些专业和学校吗？”
+“我想去周边繁华一些的学校上学，哪些学校比较适合呢？”
 
+信息检索类：
+（格式：学校1/学校2/… + 方面）
+“北邮+宿舍”
+“北大/上交大/南开+食堂”
+“北航/川大/西政+强势专业”
+
+请注意，我们尚未涵盖所有院校，你可以提问“目前支持哪些学校？”来获取院校名单。
+“方面”主要包含：学业（专业情况、竞赛、社会实践、转专业政策），生活（宿舍、运动、食堂、社团、学校周边）等内容。
+
+我们的数据均来自在校学生原创内容，可能与客观情况有所偏差，请汇总多方信息后再做志愿填报的决定，希望我们初出茅庐的产品可以对你有所帮助～`
+
+function App() {
   interface Message {
     content: string;
     isUser: boolean;
   }
+
+  const initalMessages: Message[] = [
+    {
+      content: systemMessage,
+      isUser: false,
+    }
+  ];
+
+  const [inputText, setInputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>(initalMessages);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value);
@@ -50,25 +76,25 @@ function App() {
 
     try {
       // Send user's question to the API
-      const response = await fetch('https://api.scholarcompass.org/query', {
-        method: 'POST',
-        body: JSON.stringify({ question: inputText }),
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-      });
+      // const response = await fetch('https://api.scholarcompass.org/query', {
+      //   method: 'POST',
+      //   body: JSON.stringify({ question: inputText }),
+      //   headers: {
+      //     'Content-Type': 'text/plain',
+      //   },
+      // });
     
-      if (response) {
-        const data = await response.json();
+      // if (response) {
+      //   const data = await response.json();
     
-        // Display API response
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { content: data.answer, isUser: false },
-        ]);
-      } else {
-        console.error('Request failed');
-      }
+      //   // Display API response
+      //   setMessages((prevMessages) => [
+      //     ...prevMessages,
+      //     { content: data.answer, isUser: false },
+      //   ]);
+      // } else {
+      //   console.error('Request failed');
+      // }
     } catch (error) {
       console.error('Request failed with error:', error);
     } finally {
