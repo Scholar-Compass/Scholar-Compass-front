e1a559d6bcb8e3efccc2dbb883c5ca273e7f31aa
Astatine-213-Tian <astatine213.tian@gmail.com>
2023-06-14 23:44:47 -0400

    refactor: rename titlebar to navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,26 @@
+import { BoxProps, Flex, Text } from "@chakra-ui/react";
+
+const NavBar = (
+  props: BoxProps
+) => {
+  return (
+    <Flex
+      p={4}
+      bg="#13253F"
+      color="white"
+      justify="center"
+      align="center"
+      width="100%"
+      pos="sticky"
+      top={0}
+      zIndex={999}
+      {...props}
+    >
+      <Text fontSize="2xl">
+        Scholar Compass
+      </Text>
+    </Flex>
+  )
+}
+
+export default NavBar;
