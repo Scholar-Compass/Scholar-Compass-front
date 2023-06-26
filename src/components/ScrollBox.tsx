55e6e92b78750d3bef88e03cf79970bb8e3d5019
Astatine-213-Tian <astatine213.tian@gmail.com>
2023-06-26 05:22:43 -0400

    fix: autoscroll on last box height changes

--- src/components/ScrollBox.tsx
+++ src/components/ScrollBox.tsx
@@ -1,15 +1,36 @@
 import { Box, BoxProps } from '@chakra-ui/react';
 import { useEffect, useRef } from 'react';
 
-const ScrollBox = ({ children, ...props }: BoxProps) => {
+type ScrollBoxProps = {
+  children: React.ReactNode;
+} & BoxProps;
+
+const ScrollBox = ({ children, ...props }: ScrollBoxProps) => {
+  const boxRef = useRef<HTMLDivElement>(null);
   const endRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    endRef.current?.scrollIntoView({ behavior: 'smooth' });
+  let boxHeight = boxRef.current?.clientHeight || 0;
+  const resizeObserver = new ResizeObserver(entries => {
+    const newHeight = entries[0].target.clientHeight;
+    if (newHeight !== boxHeight) {
+      boxHeight = newHeight;
+      endRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
   });
 
+  useEffect(
+    () => {
+      resizeObserver.observe(boxRef.current!);
+      return () => {
+        resizeObserver.disconnect();
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
   return (
-    <Box overflowY="auto" {...props}>
+    <Box ref={boxRef} overflowY="auto" {...props}>
       {children}
       <Box ref={endRef} />
     </Box>
