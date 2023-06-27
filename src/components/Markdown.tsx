401586c63693c36cdbdbca54522ace04a598ab9d
Astatine-213-Tian <astatine213.tian@gmail.com>
2023-06-27 01:36:37 -0400

    fix(ui): modify external link icon position

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,11 +1,12 @@
 import {
   Icon,
   Link,
+  Text,
   ListItem,
   OrderedList,
   UnorderedList,
 } from '@chakra-ui/react';
-import { AiOutlineLink } from 'react-icons/ai';
+import { BiLinkExternal } from 'react-icons/bi';
 import ReactMarkdown, { Components } from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { memo } from 'react';
@@ -20,8 +21,10 @@ const components: Components = {
   li: ({ children }) => <ListItem>{children}</ListItem>,
   a: ({ children, href }) => (
     <Link href={href} isExternal color="blue.500">
-      {children}
-      <Icon ml="2px" as={AiOutlineLink} />
+      <Text as="span" pos="relative" top="1px">
+        {children}
+      </Text>
+      <Icon ml="2px" verticalAlign="text-bottom" as={BiLinkExternal} />
     </Link>
   ),
 };
